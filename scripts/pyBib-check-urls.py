4e06b3ebf0978689862bd2988872db12e6c79fe1
Von Welch <von@vwelch.com>
2012-06-20 21:32:28 -0400

    pyBib-check-urls.py: whitespace clean up.

--- scripts/pyBib-check-urls.py
+++ scripts/pyBib-check-urls.py
@@ -18,9 +18,9 @@ def get_entry_datetime(entry):
     s = entry["month"] if entry["month"] else "Jan"
     s += " " + entry["year"]
     try:
-        d = datetime.datetime.strptime(s, "%b %Y")
+	d = datetime.datetime.strptime(s, "%b %Y")
     except ValueError:
-        d = datetime.datetime.strptime(s, "%B %Y")
+	d = datetime.datetime.strptime(s, "%B %Y")
     return d
 
 def parse_bib(filenames, append_to=None):
@@ -28,19 +28,19 @@ def parse_bib(filenames, append_to=None):
 
     filenames can be a single string instead of a list."""
     if append_to is not None:
-        bibs = append_to
+	bibs = append_to
     else:
-        bibs = [] 
+	bibs = []
     config = ConfigParser.SafeConfigParser()
     config.read(filenames)
     sections = config.sections()
     for section in sections:
-        entry = dict(config.items(section))
-        entry["key"] = section
-        entry.setdefault("month", None)
-        entry.setdefault("howpublished", None)
-        entry["datetime"] = get_entry_datetime(entry)
-        bibs.append(entry)
+	entry = dict(config.items(section))
+	entry["key"] = section
+	entry.setdefault("month", None)
+	entry.setdefault("howpublished", None)
+	entry["datetime"] = get_entry_datetime(entry)
+	bibs.append(entry)
     # Sort descending by datetime
     bibs.sort(cmp=lambda a,b: cmp(b["datetime"], a["datetime"]))
     return bibs
@@ -49,8 +49,8 @@ def parse_bib(filenames, append_to=None):
 
 def check_url(url):
     response = requests.get(url,
-                            verify=False  # Ignore https certificates
-                            )
+			    verify=False  # Ignore https certificates
+			    )
     return response.status_code == 200
 
 ######################################################################
@@ -59,37 +59,35 @@ def main(argv=None):
     # Do argv default this way, as doing it in the functional
     # declaration sets it at compile time.
     if argv is None:
-        argv = sys.argv
+	argv = sys.argv
 
     # Argument parsing
     parser = argparse.ArgumentParser(
-        description=__doc__, # printed with -h/--help
-        # Don't mess with format of description
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        # To have --help print defaults with trade-off it changes
-        # formatting, use: ArgumentDefaultsHelpFormatter
-        )
+	description=__doc__, # printed with -h/--help
+	# Don't mess with format of description
+	formatter_class=argparse.RawDescriptionHelpFormatter,
+	# To have --help print defaults with trade-off it changes
+	# formatting, use: ArgumentDefaultsHelpFormatter
+	)
     parser.add_argument("-v", "--verbose",
-                        action='store_const', dest='verbose',
-                        const=True, default=False,
-                        help="run verbosely")
+			action='store_const', dest='verbose',
+			const=True, default=False,
+			help="run verbosely")
     parser.add_argument('bibs', metavar='args', type=str, nargs='+',
-                        help='bib files to use')
+			help='bib files to use')
     args = parser.parse_args()
     entries = parse_bib(args.bibs)
     status = 0
     for entry in entries:
-        if entry.has_key("url"):
-            url = entry["url"]
-            if check_url(url):
-                if args.verbose:
-                    print "{} ... GOOD".format(url)
-            else:
-                print "{} ... BAD".format(url)
-                status = 1
+	if entry.has_key("url"):
+	    url = entry["url"]
+	    if check_url(url):
+		if args.verbose:
+		    print "{} ... GOOD".format(url)
+	    else:
+		print "{} ... BAD".format(url)
+		status = 1
     return(status)
 
 if __name__ == "__main__":
     sys.exit(main())
-
-    
