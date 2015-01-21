331788c4b7becfd414febb369cc1a303bf7dddb9
Von Welch <von@vwelch.com>
2015-01-21 16:19:19 -0500

    pyBib-check-urls.py: whitespace cleanup

--- scripts/pyBib-check-urls.py
+++ scripts/pyBib-check-urls.py
@@ -13,47 +13,49 @@ from pybib import BibParser
 
 ######################################################################
 
+
 def check_url(url):
     response = requests.get(url,
-			    verify=False  # Ignore https certificates
-			    )
+                            verify=False  # Ignore https certificates
+                            )
     return response.status_code == 200
 
 ######################################################################
+
 
 def main(argv=None):
     # Do argv default this way, as doing it in the functional
     # declaration sets it at compile time.
     if argv is None:
-	argv = sys.argv
+        argv = sys.argv
 
     # Argument parsing
     parser = argparse.ArgumentParser(
-	description=__doc__, # printed with -h/--help
-	# Don't mess with format of description
-	formatter_class=argparse.RawDescriptionHelpFormatter,
-	# To have --help print defaults with trade-off it changes
-	# formatting, use: ArgumentDefaultsHelpFormatter
-	)
+        description=__doc__,  # printed with -h/--help
+        # Don't mess with format of description
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        # To have --help print defaults with trade-off it changes
+        # formatting, use: ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument("-v", "--verbose",
-			action='store_const', dest='verbose',
-			const=True, default=False,
-			help="run verbosely")
+                        action='store_const', dest='verbose',
+                        const=True, default=False,
+                        help="run verbosely")
     parser.add_argument('bibs', metavar='args', type=str, nargs='+',
-			help='bib files to use')
+                        help='bib files to use')
     args = parser.parse_args()
     bib_parser = BibParser()
     entries = bib_parser.parse_bib(args.bibs)
     status = 0
     for entry in entries:
-	if entry.has_key("url"):
-	    url = entry["url"]
-	    if check_url(url):
-		if args.verbose:
-		    print "{} ... GOOD".format(url)
-	    else:
-		print "{} ... BAD".format(url)
-		status = 1
+        if entry.has_key("url"):
+            url = entry["url"]
+            if check_url(url):
+                if args.verbose:
+                    print "{} ... GOOD".format(url)
+                else:
+                    print "{} ... BAD".format(url)
+                    status = 1
     return(status)
 
 if __name__ == "__main__":
